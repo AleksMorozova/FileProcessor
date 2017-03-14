ec48d82dc49b7284083f54edb5520a8642f493db
AleksMorozova <aleks_morozova91@mail.ru>
2017-03-14 21:41:51 +0200

    remove unused code

--- FileTest/FileTest/MainProcessor.cs
+++ FileTest/FileTest/MainProcessor.cs
@@ -20,46 +20,13 @@ namespace FileTest
         {
             if (Directory.Exists(Program.startFolder))
             {
-                // This path is a directory
-                //ProcessDirectory(Program.startFolder);
                 Process(Program.startFolder);
-               /* var concurrentBag = new ConcurrentBag<Task>();
-                var concurrentBag2 = new ConcurrentBag<string>();
-                ProcessDir(Program.startFolder, concurrentBag, concurrentBag2);
-                Task.WaitAll(concurrentBag.ToArray());*/
             }
             else
             {
                 Console.WriteLine("{0} is not a valid file or directory.", Program.startFolder);
             }
 
-        }
-
-        // Process all files in the directory passed in, recurse on any directories 
-        // that are found, and process the files they contain.
-
-        public void ProcessDirectory(string targetDirectory)
-        {
-            var fileEntries = Directory.GetFiles(targetDirectory);
-            Task.WaitAll(fileEntries.Select(fileName => Task.Run(() => _processor.ProcessFile(fileName))).ToArray());
-
-            var subdirectoryEntries = Directory.GetDirectories(targetDirectory);
-            Task.WaitAll(subdirectoryEntries.Select(subdirectory => Task.Run(() => ProcessDirectory(subdirectory))).ToArray());
-        }
-
-        public void ProcessDirectory2(string targetDirectory)
-        {
-            var fileEntries = Directory.GetFiles(targetDirectory);
-            var subdirectoryEnries = Directory.GetDirectories(targetDirectory);
-
-        }
-
-        public void ProcessDir(string target, ConcurrentBag<Task> fileTasks, ConcurrentBag<string> fileNames)
-        {
-            foreach(var f in Directory.GetFiles(target)) { fileNames.Add(f); }
-            var dirs = Directory.GetDirectories(target);
-            var tasks = dirs.Select(s => Task.Run(() => ProcessDir(s, fileTasks, fileNames)));
-            foreach (var t in tasks) fileTasks.Add(t);
         }
 
         private Task<IEnumerable<string>> GetFiles(string path)
